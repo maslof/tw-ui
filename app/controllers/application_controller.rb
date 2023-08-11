a26834136c6be5ca3e97ca1b2a1aded543229e4a
Maslov Anton <daniladobre@gmail.com>
2023-08-11 15:43:45 +0300

    application templates hone, details, settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   def index
+  end
 
+  def details
+  end
+
+  def settings
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "/details", to: "application#details"
+  get "/settings", to: "application#settings"
+
+  root "application#index"
 end
